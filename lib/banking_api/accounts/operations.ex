62394c760e56b4cedfb083ba9d6fe420d61aaf29
Joao Pedro <jpresende502@gmail.com>
2021-04-21 21:54:46 -0300

    feat: Added new module to run operations functions.

---
+++ lib/banking_api/accounts/operations.ex
@@ -0,0 +1,12 @@
+defmodule BankingApi.Account.Operations do 
+  alias Ecto.Multi
+
+  alias BankingApi.{Repo, Account}
+
+  @moduledoc """
+    This module is for methods of account, make deposits, withdraws, transactions
+  """
+
+  def deposit(account_uuid) do 
+  end
+end
