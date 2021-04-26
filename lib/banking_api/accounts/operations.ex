b6831581d37c16e0c3c5782dd843f3ab591bc048
Joao Pedro <jpresende502@gmail.com>
2021-04-26 19:20:14 -0300

    feat: Finishing banking operations

--- lib/banking_api/accounts/operations.ex
+++ lib/banking_api/accounts/operations.ex
@@ -1,12 +1,82 @@
-defmodule BankingApi.Account.Operations do 
-  alias Ecto.Multi
-
-  alias BankingApi.{Repo, Account}
+defmodule BankingApi.Account.Operations do
+  alias BankingApi.{Repo, Account, User}
 
   @moduledoc """
     This module is for methods of account, make deposits, withdraws, transactions
   """
 
-  def deposit(account_uuid) do 
+  def deposit(account_uuid, quantity) do
+    {:ok, account} = get_account(account_uuid)
+    quantity =
+      quantity
+      |> to_string()
+      |> Decimal.new()
+    new_balance = Decimal.add(account.balance, quantity)
+
+    account
+    |> Account.changeset(%{balance: new_balance})
+    |> Repo.update!()
+
+    {:ok, "The balance of account has been updated"}
+  end
+
+  def withdraw(account_uuid, quantity) do
+    {:ok, account} = get_account(account_uuid)
+
+    quantity =
+      quantity
+      |> to_string()
+      |> Decimal.new()
+
+    new_balance = Decimal.sub(account.balance, quantity)
+
+    account
+    |> Account.changeset(%{balance: new_balance})
+    |> Repo.update!()
+
+    {:ok, "The balance of account has been updated"}
+  end
+
+  def transaction(sender_uuid, receiver_uuid, quantity) do
+    {:ok, sender_account} = get_account(sender_uuid)
+    {:ok, receiver_account} = get_account(receiver_uuid)
+
+    quantity =
+      quantity
+      |> to_string()
+      |> Decimal.new()
+
+    sender_new_balance = Decimal.sub(sender_account.balance, quantity)
+    receiver_new_balance = Decimal.add(receiver_account.balance, sender_new_balance)
+
+    receiver_account
+    |> Account.changeset(%{balance: receiver_new_balance})
+    |> Repo.update!()
+
+    {:ok, receiver_name} = get_name_account(receiver_uuid)
+
+    {:ok, "The transaction was successful to #{receiver_name.name}"}
+  end
+
+  defp get_account(uuid) do
+    try do
+      account_result = Repo.get_by(Account, user_id: uuid)
+
+      {:ok, account_result}
+    rescue
+      Ecto.NoResultsError ->
+        {:error, "Non found results of this Account UUID"}
+    end
+  end
+
+  defp get_name_account(uuid) do
+    try do
+      user_result = Repo.get(User, uuid)
+
+      {:ok, user_result}
+    rescue
+      Ecto.NoResultsError ->
+        {:error, "Non found results of this User UUID"}
+    end
   end
 end
