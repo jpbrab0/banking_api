438af4e00dbcc7280174f2a247e21ed85b4e0961
Joao Pedro <jpresende502@gmail.com>
2021-04-26 19:19:01 -0300

    feat: Finishing methods and updating starter account balance.

--- lib/banking_api/users/methods.ex
+++ lib/banking_api/users/methods.ex
@@ -1,4 +1,4 @@
-defmodule BankingApi.User.Methods do 
+defmodule BankingApi.User.Methods do
   alias Ecto.Multi
   alias BankingApi.{Repo, Account, User}
 
@@ -6,7 +6,7 @@ defmodule BankingApi.User.Methods do
     This module is for run databases functions, for exemple, create users, view balance of your account and anouther functions.
   """
 
-  def get_balance(user_uuid) do 
+  def get_balance(user_uuid) do
     try do
       result = Repo.get_by!(Account, user_id: user_uuid)
 
@@ -17,23 +17,23 @@ defmodule BankingApi.User.Methods do
     end
   end
 
-  def insert_user(params) do 
+  def insert_user(params) do
     Multi.new()
     |> Multi.insert(:create_user, User.changeset(params))
     |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user)  end)
     |> run_transaction()
   end
-  
-  defp insert_account(repo, user) do 
+
+  defp insert_account(repo, user) do
     user.id
     |> account_changeset()
     |> repo.insert()
   end
 
-  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "100.00"})
-  
-  defp run_transaction(multi) do 
-    case Repo.transaction(multi) do 
+  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "1000.00"})
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
       {:ok, %{create_account: account}} -> {:ok, account}
     end
