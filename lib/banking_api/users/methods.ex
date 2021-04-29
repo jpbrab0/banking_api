78c119981466e1eebfa4839513f2a5301c4a9164
Joao Pedro <jpresende502@gmail.com>
2021-04-29 14:25:42 -0300

    feat: Changing the success message.

--- lib/banking_api/users/methods.ex
+++ lib/banking_api/users/methods.ex
@@ -35,7 +35,7 @@ defmodule BankingApi.User.Methods do
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, _reason, _changes} -> {:error, "Ocurred an error in the database"}
-      {:ok, %{create_account: _account}} -> {:ok, "The user has been created"}
+      {:ok, %{create_account: account}} -> {:ok, "The user has been created", account.id}
     end
   end
 end
