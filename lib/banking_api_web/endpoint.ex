3a7603bebb9a7c74f19211a1ebef1107793c0dd5
Joao Pedro <jpresende502@gmail.com>
2021-04-30 11:31:11 -0300

    fix: Removing comments in endpoint.ex

--- lib/banking_api_web/endpoint.ex
+++ lib/banking_api_web/endpoint.ex
@@ -1,9 +1,6 @@
 defmodule BankingApiWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :banking_api
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
   @session_options [
     store: :cookie,
     key: "_banking_api_key",
@@ -16,18 +13,12 @@ defmodule BankingApiWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
     from: :banking_api,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     plug Phoenix.CodeReloader
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :banking_api
